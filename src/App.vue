e4eb76668c5103861af8ec64d4b1853d6bba1007
bryan_v <bryan.villafuerte@bryvil.com>
2020-03-07 11:51:38 +0100

    installed bootstrap and added header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <componentHeader></componentHeader>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import componentHeader from "./components/componentHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    componentHeader
   }
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/componentHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container header-title">
+    <h1 class="display-2">JS Frameworks MA2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "componentHeader"
+};
+</script>
+
+<style scoped lang="scss">
+.header-title {
+  padding: 2rem;
+
+  h1 {
+    text-align: center;
+  }
+}
+</style>
